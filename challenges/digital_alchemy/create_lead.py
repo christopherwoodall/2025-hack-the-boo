174334cd1677578cd60d668edd3b2301188dde44
Chris <363708+christopherwoodall@users.noreply.github.com>
2025-10-26 21:36:10 -0500

    Finalize content

--- challenges/digital_alchemy/create_lead.py
+++
@@ -1,5 +0,0 @@
-with open('lead.txt', 'wb') as f:
-    f.write(b'MTRLLEAD')
-    f.write(b'\x97\x2c\xff\xbc')
-    f.write(b'SPIRITUS_CODICIS_EXPERGISCERE')
-    f.write(b'MPF~Sq={=tZ6k^_j:VE4k9RFv4b7a2hu')
